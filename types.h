49603872cd8b2d16186ad0db598e82d912cb2fee
Justin Evankovich <justinevankovich@gmail.com>
2017-01-21 23:27:00 -0500

    added .gitignore and some comments to strset

--- types.h
+++ types.h
@@ -28,10 +28,25 @@ enum token {
 	QUESTION_MARK, // ?
 };
 
+struct set_entry {
+	uint64_t hash;
+	char *str;
+};
+
 struct strset {
 	// TODO: Implement this
 	// Should have a private heap for strings
-	// Since hash collisions are (should be) rare, 
+	// since hash collisions are (should be) rare, 
+	
+	// When size/capacity exceeds some threshold, we need to expand.
+	// Expanding sounds like a pain
+
+	size_t size; // The number of entries in the heap
+	size_t capacity; // The total number of possible entries in the heap
+	struct set_entry *entries; // An array of the entries in the set
+	size_t heap_capacity; // The current capacity of the heap
+	char *heap; // A pointer to the beginning of the heap
+	char *next_heap; // The next location in the heap available for a string
 };
 
 struct lex_state {
